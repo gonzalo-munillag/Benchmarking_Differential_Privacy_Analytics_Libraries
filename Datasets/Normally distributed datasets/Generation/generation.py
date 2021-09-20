278b3d84607d7f3d96cb9649517ea7f101173a27
Gonzalo Munilla Garrido <57599753+gonzalo-munillag@users.noreply.github.com>
2021-09-20 22:26:11 +0200

    Update generation.py

--- Datasets/Normally distributed datasets/Generation/generation.py
+++ Datasets/Normally distributed datasets/Generation/generation.py
@@ -4,17 +4,15 @@ import numpy as np
 from scipy.stats import skewnorm
 import itertools
 
-# ## Goal: We would like to create different arrays that represent different characteristic combinations in terms of scale (number of elements, sample size), domain (the range of values the elements of the array take, std), and shape (how imbalanced the dataset is, skewness)
+# ## Goal: We would like to create different arrays that represent different characteristic combinations in terms of dataset size (number of elements, sample size), spread (the range of values the elements of the array take, scale), and shape (how symmetric the dataset is, skewness)
 
 # ## Levels:
-# ### Scale: 1000, 10000, 100000
-# ### Domain: std= 50, std=250, std=500
+# ### Dataset size: 1000, 10000, 100000
+# ### Spread: scale= 50, scale=250, scale=500
 # ### Shape: skew = 0, skew = 5, skew = 50
-# ## We define three levels for each of these terms, yielding 27 possible combinations. We will later on apply differencial privacy mechanism to the arrays to test how the error is affected by these characteristics (scale, domain, shape).
-# ## This notebook only shows how to tune each of the parameters of a normal distribution created with scipy 
-# ### Funciton to be used:  skewnorm.rvs(a => for skewness, loc=> for mean, scale => for std, size => scale)
-# ### Let us not confuse the term SCALE from the beginning with the input of the function called scale, which means std
-# ### Here is how you change the SCALE, the number of samples (size input)
+# ## We define three levels for each of these terms, yielding 27 possible combinations. We will later on apply differencial privacy mechanism to the arrays to test how the error is affected by these characteristics (dataset size, scale, skew).
+# ## This notebook only shows how to tune each of the parameters of a skew-normal distribution created with Scipy 
+# ### Funciton to be used:  skewnorm.rvs(a => for skewness, loc=> for location, scale => for scale, size => datset size)
 
 def dataset_generation(a, scale, size, f):
     data= skewnorm.rvs(a = a, loc=0, scale= scale, size=size)
